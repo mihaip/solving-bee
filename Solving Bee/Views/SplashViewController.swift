32cabd20e3e6c0f5aa34118d242c94008b7539e5
Mihai Parparita <mihai@persistent.info>
2021-03-05 10:41:02 -0800

    Fix splash view flashing when transition from the word list to an individual word

--- Solving Bee/Views/SplashViewController.swift
+++ Solving Bee/Views/SplashViewController.swift
@@ -8,11 +8,17 @@ class SplashViewController : UIHostingController<SplashView> {
             // Though a bit strange, the easiest way to do a cross-fade
             // animation is to present the scanning controler modally.
             let scanningViewController = UINavigationController(rootViewController: ScanningViewController(dismiss: {
+                self.view.isHidden = false
                 self.dismiss(animated: true, completion: nil)
             }))
             scanningViewController.modalPresentationStyle = .overFullScreen
             scanningViewController.modalTransitionStyle = .crossDissolve
-            self.present(scanningViewController, animated: true, completion: nil)
+            self.present(scanningViewController, animated: true, completion: {
+                // Make sure that we're not visible when doing word view
+                // transitions (during which time transparent views may be
+                // on-screen).
+                self.view.isHidden = true
+            })
         }
     }
 
